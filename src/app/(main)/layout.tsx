aba51377be39d1f9fd2ab326a1fb4254ea91bc31
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-06-03 13:14:41 +0200

    feat: auth-layoout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react'
-import { Header } from '@/widgets/header'
 import styles from './layout.module.scss'
 import '@/shared/config/styles/index.scss'
 import Sidebar from '@/widgets/Sidebar/ui/Sidebar/Sidebar'
@@ -8,9 +7,6 @@ import MobileSideBar from '@/widgets/Sidebar/ui/MobileSideBar/MobileSideBar'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.stickyHeader}>
-        <Header />
-      </div>
       <div className={styles.contentArea}>
         <div className={styles.leftSidebar}>
           <Sidebar />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
 import './globals.css'
 import { ReactNode } from 'react'
-import styles from './layout.module.css'
+import styles from './layout.module.scss'
 import '@/shared/config/styles/index.scss'
 import { inter } from '../../public/fonts/inter'
+import { Header } from '@/widgets/header'
 // eslint-disable-next-line react-refresh/only-export-components
 export { metadata } from './metadata'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ru" className={inter.variable}>
+    <html lang="en" className={inter.variable}>
       <body className={styles.root}>
-        <main className={styles.main}>{children}</main>
+        <main className={styles.main}>
+          <div className={styles.stickyHeader}>
+            <Header />
+          </div>
+          {children}
+        </main>
       </body>
     </html>
   )
