4c7b2813a1d61a40e6b703218b6f3021fd1cbfa8
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2025-06-07 21:02:49 +0200

    feat(WQ1-102): signUp server

---
+++ src/features/auth/model/authApi.ts
@@ -0,0 +1,16 @@
+import { baseApi } from '@/shared/api/baseApi'
+import { RegisterRequest } from '@/features/auth/model/types'
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: builder => ({
+    register: builder.mutation<void, RegisterRequest>({
+      query: body => ({
+        url: '/auth/registration',
+        method: 'POST',
+        body,
+      }),
+    }),
+  }),
+})
+
+export const { useRegisterMutation } = authApi
