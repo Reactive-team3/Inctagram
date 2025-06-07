e48f1e97b392d4842fc3c9a649a7c824e2592001
SergeyStarchenko <pdxserg@gmail.com>
2025-06-07 13:51:49 -0700

    feat: sign in page UI (no logic yet)

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,34 +1,20 @@
 import React from 'react'
-import Image from 'next/image'
-import { Input } from '@/shared/ui/Input/Input'
-import { Button } from '@/shared/ui/button/Button'
-import Link from 'next/link'
 import styles from './page.module.scss'
+import { Typography } from '@/shared/ui/typography/Typography'
+import { Cards } from '@/shared/ui/cards/Cards'
+import { SignUpSocialButtons } from '@/features/ui/signUpSocialButtons/SignUpSocialButtons'
+import { SignInForm } from '@/features/ui/signInForm/SignInForm'
 
 const SingIn = () => {
   return (
-    <div className={styles.container}>
-      <h1>SingIn</h1>
-      <div className={styles.socialLink}>
-        <Image src={'/icons/google.svg'} alt={'Git hub'} width={36} height={36} />
-        <Image src={'/icons/github.svg'} alt={'Git hub'} width={36} height={36} />
-      </div>
-      <div>
-        <Input name={'Email'} label={'Email'} />
-        <Input name={'Password'} label={'Password'} />
-      </div>
-      <div className={styles.buttons}>
-        <Button as={Link} href="/signup" variant={'text'} style={{ color: 'grey' }}>
-          Forgot Password
-        </Button>
-        <Button>Sign In</Button>
-        <Button as={Link} href="/signup" variant={'text'}>
-          Sign up!
-        </Button>
-        <Button as={Link} href="/signup" variant={'text'} style={{ color: 'white' }}>
-          Don’t have an account?
-        </Button>
-      </div>
+    <div>
+      <Cards>
+        <Typography as="h1" variant="h1" className={styles.title}>
+          Sign In
+        </Typography>
+        <SignUpSocialButtons />
+        <SignInForm />
+      </Cards>
     </div>
   )
 }
