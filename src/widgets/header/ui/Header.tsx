b83b55ab4914e79d88d4764650c169d1413937b2
SergeyStarchenko <pdxserg@gmail.com>
2025-06-20 22:37:13 -0700

    chore: naming, styles

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -38,7 +38,7 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <span className={styles.name}>Instagram</span>
-      <div className="flex items-center gap-4">
+      <div className={styles.buttons}>
         <SelectComponent
           defaultValue={selectedLanguage}
           onChangeAction={handleLanguageChange}
