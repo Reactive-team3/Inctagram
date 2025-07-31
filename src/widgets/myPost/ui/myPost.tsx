e50d5a5c22f0e98936e77fd5e3c5f000d7ef595f
masterPeaceJs <bert3331@mail.ru>
2025-07-31 18:56:56 +0300

    feat(component): update post pre-final

--- src/widgets/myPost/ui/myPost.tsx
+++ src/widgets/myPost/ui/myPost.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Image from 'next/image'
 import { Typography } from '@/shared/ui/typography/Typography'
 import { Modal } from '@/shared/ui/modal/Modal'
@@ -10,6 +10,11 @@ import styles from './myPost.module.scss'
 import { DropdownMenu } from '@/shared/ui/dropdownMenu/dropdownMenu'
 import { useSelector } from 'react-redux'
 import { selectUser } from '@/shared/model/user/userSlice'
+import { useUpdatePostMutation } from '@/features/postApi/model/postApi'
+import { TextArea } from '@/shared/ui/textArea/TextArea'
+import { Button } from '@/shared/ui/button/Button'
+import { ConfirmUpdatePostModal } from '@/features/ui/updatePostModal/confirmUpdatePostModal/ConfirmUpdatePostModal'
+import { UpdatePostModal } from '@/features/ui/updatePostModal/UpdatePostModal'
 
 type Post = {
   id: number
@@ -20,6 +25,7 @@ type Post = {
 type MyPostProps = {
   isOpen: boolean
   onClose: () => void
+  onEdit: boolean
   posts: Post[]
   currentIndex: number
   onIndexChange: (index: number) => void
@@ -30,12 +36,20 @@ type MyPostProps = {
 const MyPost = ({
   isOpen,
   onClose,
+  onEdit,
   posts,
   currentIndex,
   onIndexChange,
-  onEditPost,
   onDeletePost,
 }: MyPostProps) => {
+  const [Edit, setEdit] = useState(onEdit)
+  const [inputValue, setInputValue] = useState('')
+  const [openUpdateModal, setOpenUpdateModal] = useState(false)
+  const [openConfirmUpdateModal, setOpenConfirmUpdateModal] = useState(false)
+  useEffect(() => {
+    setEdit(onEdit)
+  }, [onEdit])
+  const [updatePost] = useUpdatePostMutation()
   const userMe = useSelector(selectUser)
   // Подготавливаем слайды для всех постов
   const slides = useMemo(() => {
@@ -62,12 +76,36 @@ const MyPost = ({
     }))
   }, [posts])
 
-  const handleEditPost = () => {
-    const currentPost = posts[currentIndex]
-    if (currentPost && onEditPost) {
-      onEditPost(currentPost.id)
-    }
-  }
+  const slidesForUpdate = useMemo(() => {
+    return posts.map((post, index) => ({
+      id: `post-${post.id}`,
+      content: (
+        <div>
+          {/* Основное изображение поста */}
+          {post.imageUrl && post.imageUrl[0] ? (
+            <Image
+              src={post.imageUrl[0]}
+              alt={post.description || `Post ${index + 1}`}
+              width={490}
+              height={503}
+              className={styles.imagUpdateModal}
+            />
+          ) : (
+            <Typography as="span" variant="body2">
+              No image available
+            </Typography>
+          )}
+        </div>
+      ),
+    }))
+  }, [posts])
+
+  // const handleEditPost = () => {
+  //   const currentPost = posts[currentIndex]
+  //   if (currentPost && onEditPost) {
+  //     onEditPost(currentPost.id)
+  //   }
+  // }
 
   const handleDeletePost = () => {
     const currentPost = posts[currentIndex]
@@ -76,11 +114,93 @@ const MyPost = ({
     }
   }
 
+  const handleEditPostText = () => {
+    const currentPost = posts[currentIndex]
+    const id = currentPost.id
+    if (currentPost) {
+      updatePost({ id, description: inputValue })
+      setEdit(!Edit)
+      closeConfirmUpModal()
+      closeModal()
+    }
+  }
+  const handleTextareaChange = (value: string) => {
+    setInputValue(value)
+  }
+  const openModal = () => {
+    setOpenUpdateModal(true)
+  }
+  const closeModal = () => {
+    setOpenUpdateModal(false)
+  }
+  const openConfirmUpModal = () => {
+    setOpenConfirmUpdateModal(true)
+  }
+  const closeConfirmUpModal = () => {
+    setOpenConfirmUpdateModal(false)
+  }
+
   if (!isOpen || slides.length === 0) {
     return null
   }
 
   const currentPost = posts[currentIndex]
+
+  const childrenForUpdateModal = (
+    <div style={{ display: 'flex', marginTop: '12px' }}>
+      <div className={styles.wrapperSlider}>
+        <Slider
+          slides={slidesForUpdate}
+          currentIndex={currentIndex}
+          onIndexChangeAction={onIndexChange}
+          showDots={slides.length > 1}
+          customStyles={{
+            sliderArrowPrev: styles.customArrowPrev,
+            sliderArrowNext: styles.customArrowNext,
+          }}
+        />
+      </div>
+      <div className={styles.informationBlockPost}>
+        <div className={styles.updateMainTextArea}>
+          <div className={styles.updateWrapper}>
+            <div className={styles.userPhoto}>
+              <Image src="/user-images/image.png" alt="photo" width={36} height={36} />
+            </div>
+            <Typography as="span" variant="h3" className={styles.userUrl}>
+              {userMe?.username}
+            </Typography>
+          </div>
+          <div className={styles.textAreaContainer}>
+            <TextArea
+              label="Add publication descriptions"
+              name="Add publication descriptions"
+              maxLength={500}
+              width="100%"
+              initialValue={currentPost.description}
+              onChange={value => handleTextareaChange(value)}
+            />
+            <Button onClick={openConfirmUpModal}>save change</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+  const childrenForConfirmUpdate = (
+    <div>
+      <Typography variant={'body1'} className={styles.text}>
+        Do you really want to close the edition of the publication? If you close changes won’t be
+        saved
+      </Typography>
+      <div className={styles.btn_field}>
+        <Button variant={'outline'} onClick={handleEditPostText} className={styles.btn_1}>
+          Yes
+        </Button>
+        <Button variant={'primary'} onClick={closeConfirmUpModal} className={styles.btn_2}>
+          No
+        </Button>
+      </div>
+    </div>
+  )
 
   return (
     <Modal
@@ -120,7 +240,7 @@ const MyPost = ({
           </div>
           <DropdownMenu
             className={styles.buttonIcon}
-            onEditClick={handleEditPost}
+            onEditClick={openModal}
             onDeleteClick={handleDeletePost}
           />
         </div>
@@ -130,6 +250,20 @@ const MyPost = ({
               {currentPost.description}
             </Typography>
           )}
+          <UpdatePostModal
+            open={openUpdateModal}
+            onClose={closeModal}
+            className={styles.modalProfile}
+          >
+            {childrenForUpdateModal}
+          </UpdatePostModal>
+          <ConfirmUpdatePostModal
+            open={openConfirmUpdateModal}
+            onClose={closeConfirmUpModal}
+            className={styles.confirmModal}
+          >
+            {childrenForConfirmUpdate}
+          </ConfirmUpdatePostModal>
         </div>
       </div>
     </Modal>
